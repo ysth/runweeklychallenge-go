affc634f1e5f02fcdc9225c18a02d945ebf81f48
Yitzchak Scott-Thoennes <sthoenna@gmail.com>
2025-11-09 13:47:02 -0500

    some tidying

--- run.go
+++ run.go
@@ -7,52 +7,52 @@ import (
     "github.com/kaptinlin/jsonschema"
 )
 
-func RunWeeklyChallenge(solution func(inputs any) (any, error), exampleInput string, schemaJson string) () {
+func RunWeeklyChallenge(runSolution func(inputs any) (any, error), inputsExample string, inputsSchemaJSON string) () {
 
     // compile the schema for json inputs args
-    schema, err := jsonschema.NewCompiler().Compile([]byte(schemaJson))
+    inputsSchema, err := jsonschema.NewCompiler().Compile([]byte(inputsSchemaJSON))
     if err != nil {
         fmt.Fprintln(os.Stderr, "Invalid jsonschema")
         os.Exit(1)
     }
 
     // run the solution for each command line arg
-    errors := false
-    for _, inputString := range os.Args[1:] {
+    inputs_error := false
+    for _, inputsJSON := range os.Args[1:] {
 
         // show the inputs
-        fmt.Printf("Input: %s\n", inputString)
+        fmt.Printf("Inputs: %s\n", inputsJSON)
 
         // validate the json input contains what we expect
-        validationResult := schema.Validate([]byte(inputString))
+        validationResult := inputsSchema.Validate([]byte(inputsJSON))
         if ! validationResult.IsValid() {
-            fmt.Printf("Error: invalid input: %v\n", validationResult.Error())
-            errors = true
+            fmt.Printf("Error: invalid inputs: %v\n", validationResult.Error())
+            inputs_error = true
             continue
         }
 
         // decode the json
         var inputs any
-        err := json.Unmarshal([]byte(inputString), &inputs)
+        err := json.Unmarshal([]byte(inputsJSON), &inputs)
         if err != nil {
-            fmt.Printf("Error: invalid json input: %v\n", err)
-            errors = true
+            fmt.Printf("Error: invalid inputs JSON: %v\n", err)
+            inputs_error = true
             continue
         }
 
         // run it and show the results
-        result, err := solution(inputs)
+        result, err := runSolution(inputs)
         if err != nil {
-            fmt.Printf("Error: %v", err)
+            fmt.Printf("Exception: %v", err)
             continue
         } else {
             fmt.Printf("Output: %v\n", result)
         }
-	}
+    }
 
     // if an input was not as expected, show an example
-    if errors {
-        fmt.Printf("Expected arguments like '%s'\n", exampleInput)
+    if inputs_error {
+        fmt.Printf("Expected inputs arguments like '%s'\n", inputsExample)
     }
     return;
 }
@@ -60,13 +60,13 @@ func RunWeeklyChallenge(solution func(inputs any) (any, error), exampleInput str
 // some helper functions
 
 func AsInt(inputs any, key string) int {
-    inputMap := inputs.(map[string]any)
-    return int(inputMap[key].(float64))
+    inputsMap := inputs.(map[string]any)
+    return int(inputsMap[key].(float64))
 }
 
 func AsIntSlice(inputs any, key string) []int {
-    inputMap := inputs.(map[string]any)
-    inputSlice := inputMap[key].([]any)
+    inputsMap := inputs.(map[string]any)
+    inputSlice := inputsMap[key].([]any)
     var result = make([]int, len(inputSlice))
     for i, v := range inputSlice {
         result[i] = int(v.(float64))
